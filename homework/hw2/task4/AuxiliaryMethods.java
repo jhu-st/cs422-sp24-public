51d75f6fe309c131eae76ba6f1c3cb39f6f32a31
darvishdarab <mali_dd@yahoo.com>
2024-02-19 13:34:56 -0500

    added hw2 starter

---
+++ homework/hw2/task4/AuxiliaryMethods.java
@@ -0,0 +1,76 @@
+import java.util.Arrays;
+
+public class AuxiliaryMethods {
+
+    /** Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+     *
+     * @param x the inout number
+     * @return reverse of x
+     */
+    public static int reverse(int x) {
+        int rev = 0;
+        while (x > 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7))
+                return 0;
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8))
+                return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+    /** Given two strings s and t, determine if they are isomorphic.
+     * Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+     * All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+     *
+     * @param s the first string
+     * @param t the second string
+     * @return true if s1 and s2 are isomorphic, false otherwise.
+     */
+    public static boolean isIsomorphic(String s, String t) {
+
+        int[] mappingDictStoT = new int[256];
+        Arrays.fill(mappingDictStoT, -1);
+
+        int[] mappingDictTtoS = new int[256];
+        Arrays.fill(mappingDictTtoS, -1);
+
+        for (int i = 0; i < s.length(); ++i) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            // Case 1: No mapping exists in either of the dictionaries
+            if (mappingDictStoT[c1] == -1 && mappingDictTtoS[c2] == -1) {
+                mappingDictStoT[c1] = c2;
+                mappingDictTtoS[c2] = c1;
+            }
+
+            // Case 2: Either mapping doesn't exist in one of the dictionaries or Mapping exists and
+            // it doesn't match in either of the dictionaries or both
+            else if (!(mappingDictStoT[c1] == c2 || mappingDictTtoS[c2] == c1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Count odd or positive elements
+     *
+     * @param x array to search
+     * @return count of odd/positive values in x
+     * @throws NullPointerException if x is null
+     */
+    public static int oddOrPos(int[] x) {
+        int count = 0;
+        for (int i = 0; i < x.length; i++) {
+            if (x[i] % 2 == 1 || x[i] > 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
